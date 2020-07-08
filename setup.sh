8b31301a18a3be5a412ed260eb6d62ecf5c07fad
浮生若梦 <49476783+hu-snail@users.noreply.github.com>
2020-07-08 14:23:57 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+image_version=`date +%Y%m%d%H%M`;
+echo $image_version;
+# cd blog-vuepress
+git pull --rebase origin master;
+docker stop blog-vuepress;
+docker rm blog-vuepress;
+docker build -t blog-vuepress:$image_version .;
+docker images;
+docker run -p 10002:80 -d --name blog-vuepress blog-vuepress:$image_version;
+# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
+docker logs blog-vuepress;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
